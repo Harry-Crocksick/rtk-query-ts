d8ddda022e15410b2631585f33992fa6a93614c9
Harry-Crocksick <harrycrocksick1inamillion@gmail.com>
2024-03-03 14:17:13 +0630

    Added comments

--- src/api/mainApi.tsx
+++ src/api/mainApi.tsx
@@ -15,6 +15,15 @@ export const todoApiSlice = createApi({
 			// transformResponse: (response: { data: TodoTypes[] }, _meta, _arg) => response.data,
 			// transformErrorResponse: (response: { status: string | number }, _meta, _arg) => response.status,
 			providesTags: ['todos']
+			/*
+			  providesTags & invalidatesTags signatures (can be any one of belows)
+			  - ['todo']
+			  - [{type: 'todo'}]
+			  - [{type: 'todo', id: 1}]
+			  - (result, error, queryArg) => ['todo']
+			  - (result, error, queryArg) => [{type: 'todo'}]
+			  - (result, error, queryArg) => [{type: 'todo', id: 1}]
+			*/
 		}),
 		getEachPost: builder.query<TodoTypes, string>({
 			query: (id) => `/todos/${id}`,
